1db8bd70341fedc88625045a15dfb4c031ec3395
gandhirajan <gandhirajan.n@gmail.com>
2016-07-13 17:17:45 +0530

    Added for SO Post
    Added for SO Post

---
+++ platforms/android/platform_www/cordova_plugins.js
@@ -0,0 +1,17 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "pluginId": "cordova-plugin-whitelist",
+        "runs": true
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "io.phasr.cordova.plugin.itunesfilesharing": "0.0.1",
+    "cordova-plugin-whitelist": "1.0.0"
+}
+// BOTTOM OF METADATA
+});
